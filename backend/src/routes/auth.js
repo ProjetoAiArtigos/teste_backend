3e1d9351e5c915a3e5d92dd1409553022c8eb4da
Albert Reis <albert.reis@4yousee.com.br>
2024-12-08 03:19:40 -0300

    Validação de dados para users

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import AuthController from '../controllers/authController.js';
+import { validateUserRegistration, validateUserLogin } from '../middleware/validateUser.js';
 
 const router = express.Router();
 
-router.post('/register', AuthController.register);
-router.post('/login', AuthController.login);
+router.post('/register', validateUserRegistration, AuthController.register);
+router.post('/login', validateUserLogin, AuthController.login);
 
 export default router;
