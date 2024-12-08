cff2d0a9908c9be45c269a24ce390854815d1dca
Albert Reis <albert.reis@4yousee.com.br>
2024-12-08 02:21:41 -0300

    CRUD para task

---
+++ backend/src/controllers/taskController.js
@@ -0,0 +1,54 @@
+import Task from '../models/Task.js';
+
+class TaskController {
+    static async getAllTasks(req, res) {
+        try {
+            const tasks = await Task.findAll();
+            res.json(tasks);
+        } catch (error) {
+            res.status(500).json({ error: 'Error fetching tasks' });
+        }
+    }
+
+    static async createTask(req, res) {
+        try {
+            const { title, description, status } = req.body;
+            const task = await Task.create({ title, description, status });
+            return res.status(201).json(task);
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({ error: 'Error creating task' });
+        }
+    }
+
+    static async updateTask(req, res) {
+        try {
+            const { id } = req.params;
+            const { title, description, status } = req.body;
+            const task = await Task.findByPk(id);
+            if (!task) {
+                return res.status(404).json({ error: 'Task not found' });
+            }
+            await task.update({ title, description, status });
+            res.json(task);
+        } catch (error) {
+            res.status(500).json({ error: 'Error updating task' });
+        }
+    }
+
+    static async deleteTask(req, res) {
+        try {
+            const { id } = req.params;
+            const task = await Task.findByPk(id);
+            if (!task) {
+                return res.status(404).json({ error: 'Task not found' });
+            }
+            await task.destroy();
+            res.status(204).send();
+        } catch (error) {
+            res.status(500).json({ error: 'Error deleting task' });
+        }
+    }
+}
+
+export default TaskController;
---
+++ backend/src/routes/tasks.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import TaskController from '../controllers/taskController.js';
+
+const router = express.Router();
+
+router.get('/', TaskController.getAllTasks);
+router.post('/', TaskController.createTask);
+router.put('/:id', TaskController.updateTask);
+router.delete('/:id', TaskController.deleteTask);
+
+export default router;
